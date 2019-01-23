94122801f6e87a8c15ed49a91b0dd4f919585e7e
myungjaeyu <rootcode10@gmail.com>
2019-01-23 11:34:29 +0900

    refactor: Updated mock users data.

--- idP/internal/_USERS_DATA/index.js
+++ idP/internal/_USERS_DATA/index.js
@@ -3,27 +3,21 @@ module.exports = [
         id : 1,
         email : "a@example.com",
         password : "password123",
-        first_name : "Yu",
-        last_name : "Myungjae",
-        nickname : "A",
-        cash : 15000
+        first_name : "Myungjae",
+        last_name : "Yu"
     },
     {
         id : 2,
         email : "b@example.com",
         password : "bbb123",
-        first_name : "Kim",
-        last_name : "Myungjae",
-        nickname : "A",
-        cash : 15000
+        first_name : "Myungjae",
+        last_name : "Kim"
     },
     {
         id : 3,
         email : "c@example.com",
         password : "ccc123",
-        first_name : "Park",
-        last_name : "Myungjae",
-        nickname : "A",
-        cash : 15000
+        first_name : "Myungjae",
+        last_name : "Park"
     }
 ]
